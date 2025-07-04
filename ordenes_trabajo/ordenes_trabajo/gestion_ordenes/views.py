b04ea2008e3ac307e1adc9161e1d38ef0fda2177
CToph02 <christopher.munoz.0220@gmail.com>
2025-07-03 21:21:53 -0400

    First Commit

---
+++ ordenes_trabajo/ordenes_trabajo/gestion_ordenes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def saludo(request):
+    return HttpResponse("Hola")
+
+def index(request):
+    return render(request, 'index.html', {})
